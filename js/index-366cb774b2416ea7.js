d82973f01847c855271a615b0e002d2285dbdfb6
edisontkp <im@edisontkp.com>
2024-10-04 01:47:28 +0000

    fix mobile menu

--- js/index-366cb774b2416ea7.js
+++ js/index-366cb774b2416ea7.js
@@ -15,86 +15,170 @@
             return n
         }
 
-        if ((0, O.lw)().isMobile) {
-            // The full code from above goes here
-            // Inject CSS styles into the <head>
-            var style = document.createElement('style');
-            style.textContent = `
-            body {
-                font-family: Arial, Helvetica, sans-serif;
-            }
-            .mobile-container {
-                max-width: 480px;
-                margin: auto;
-                color: white;
-                border-radius: 10px;
-            }
-            .topnav {
-                overflow: hidden;
-                background-color: #333;
-                position: relative;
-            }
-            .topnav #myLinks {
-                display: none;
-            }
-            .topnav a {
-                color: white;
-                padding: 14px 16px;
-                text-decoration: none;
-                font-size: 17px;
-                display: block;
-            }
-            .topnav a.icon {
-                background: black;
-                display: block;
-                position: absolute;
-                right: 0;
-                top: 0;
-            }
-            .topnav a:hover {
-                background-color: #ddd;
-                color: black;
-            }
-            .active {
-                background-color: #04AA6D;
-                color: white;
-            }
-            `;
-            document.head.appendChild(style);
+        // if ((0, O.lw)().isMobile) {
+        //     // The full code from above goes here
+        //     // Inject CSS styles into the <head>
+        //     var style = document.createElement('style');
+        //     style.textContent = `
+        //     body {
+        //         font-family: Arial, Helvetica, sans-serif;
+        //     }
+        //     .mobile-container {
+        //         max-width: 480px;
+        //         margin: auto;
+        //         color: white;
+        //         border-radius: 10px;
+        //     }
+        //     .topnav {
+        //         overflow: hidden;
+        //         background-color: #333;
+        //         position: relative;
+        //     }
+        //     .topnav #myLinks {
+        //         display: none;
+        //     }
+        //     .topnav a {
+        //         color: white;
+        //         padding: 14px 16px;
+        //         text-decoration: none;
+        //         font-size: 17px;
+        //         display: block;
+        //     }
+        //     .topnav a.icon {
+        //         background: black;
+        //         display: block;
+        //         position: absolute;
+        //         right: 0;
+        //         top: 0;
+        //     }
+        //     .topnav a:hover {
+        //         background-color: #ddd;
+        //         color: black;
+        //     }
+        //     .active {
+        //         background-color: #04AA6D;
+        //         color: white;
+        //     }
+        //     `;
+        //     document.head.appendChild(style);
 
-            // Inject the HTML structure into the <body>
-            var mobileContainer = document.createElement('div');
-            mobileContainer.className = 'mobile-container';
-            mobileContainer.innerHTML = `
-            <div class="topnav">
-                <a href="#home" class="active">Logo</a>
-                <div id="myLinks">
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-                </div>
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-                </a>
-            </div>
-            `;
-            document.body.appendChild(mobileContainer);
+        //     // Inject the HTML structure into the <body>
+        //     var mobileContainer = document.createElement('div');
+        //     mobileContainer.className = 'mobile-container';
+        //     mobileContainer.innerHTML = `
+        //     <div class="topnav">
+        //         <a href="#home" class="active">Logo</a>
+        //         <div id="myLinks">
+        //         <a href="#news">News</a>
+        //         <a href="#contact">Contact</a>
+        //         <a href="#about">About</a>
+        //         </div>
+        //         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        //         <i class="fa fa-bars"></i>
+        //         </a>
+        //     </div>
+        //     `;
+        //     document.body.appendChild(mobileContainer);
 
-            // Inject the JavaScript function for toggling the menu
-            var script = document.createElement('script');
-            script.textContent = `
-            function myFunction() {
-                var x = document.getElementById("myLinks");
-                if (x.style.display === "block") {
-                x.style.display = "none";
-                } else {
-                x.style.display = "block";
-                }
-            }
-            `;
-            document.body.appendChild(script);
-        }
+        //     // Inject the JavaScript function for toggling the menu
+        //     var script = document.createElement('script');
+        //     script.textContent = `
+        //     function myFunction() {
+        //         var x = document.getElementById("myLinks");
+        //         if (x.style.display === "block") {
+        //         x.style.display = "none";
+        //         } else {
+        //         x.style.display = "block";
+        //         }
+        //     }
+        //     `;
+        //     document.body.appendChild(script);
+        // }
+// Detect if the user is on a mobile device
+var isMobile = window.matchMedia("only screen and (max-width: 900px)").matches;
 
+if (isMobile) {
+    // Inject meta tag for viewport into the <head>
+    
+    // Inject CSS styles into the <head>
+    var style = document.createElement('style');
+    style.textContent = `
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .mobile-container {
+        max-width: 480px;
+        margin: auto;
+        background-color: #555;
+        height: 500px;
+        color: white;
+        border-radius: 10px;
+    }
+    .topnav {
+        overflow: hidden;
+        background-color: #333;
+        position: relative;
+    }
+    .topnav #myLinks {
+        display: none;
+    }
+    .topnav a {
+        color: white;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+        display: block;
+    }
+    .topnav a.icon {
+        background: black;
+        display: block;
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+    .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+    .active {
+        background-color: #04AA6D;
+        color: white;
+    }
+    `;
+    document.head.appendChild(style);
+
+    // Inject the HTML structure into the <body>
+    var mobileContainer = document.createElement('div');
+    mobileContainer.className = 'mobile-container';
+    mobileContainer.innerHTML = `
+    <div class="topnav">
+        <a href="#home" class="active">Logo</a>
+        <div id="myLinks">
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+    `;
+    document.body.appendChild(mobileContainer);
+
+    // // Inject the JavaScript function for toggling the menu
+    // var script = document.createElement('script');
+    // script.textContent = `
+    // function myFunction() {
+    //     var x = document.getElementById("myLinks");
+    //     if (x.style.display === "block") {
+    //         x.style.display = "none";
+    //     } else {
+    //         x.style.display = "block";
+    //     }
+    // }
+    // `;
+    // document.body.appendChild(script);
+}
         function u(e) {
             for (var t = 1; t < arguments.length; t++) {
                 var n = null != arguments[t] ? arguments[t] : {};
